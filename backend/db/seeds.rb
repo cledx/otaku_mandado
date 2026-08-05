d90df8ceebf6ae668f470bf5b63e7a770309a005
carlos ledoux <carlosledoux@gmail.com>
2026-08-05 12:07:29 +0900

    Fixed seed password

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -32,7 +32,7 @@ TEXT
 end
 
 [
-  ["admin@example.com", "admin"],
+  ["admin@example.com", "admin123"],
   ["client1@example.com", "client"],
   ["client2@example.com", "client"],
   ["client3@example.com", "client"]
